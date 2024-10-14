abf5eaf793f447d6e702137d852938880f25b0d5
tducviet86 <tranducvietdtu@gmail.com>
2024-10-14 23:28:36 +0700

    update product

--- src/page/shop/Shop.jsx
+++ src/page/shop/Shop.jsx
@@ -57,21 +57,7 @@ function Shop() {
             spacing="lg"
           >
             {products.map((product, index) => (
-              <Product
-                key={index}
-                product={product}
-                style={{
-                  padding: "10px",
-                  border: "1px solid #e0e0e0",
-                  borderRadius: "12px",
-                  boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
-                  transition: "transform 0.3s ease, box-shadow 0.3s ease",
-                  "&:hover": {
-                    transform: "scale(1.05)",
-                    boxShadow: "0 6px 16px rgba(0, 0, 0, 0.2)",
-                  },
-                }}
-              />
+              <Product key={index} product={product} />
             ))}
           </Group>
         )}
