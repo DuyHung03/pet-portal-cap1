acb26b0b2f60e0cb5c89ad97faab647f214c94c7
DuyHung03 <duyhung180903@gmail.com>
2024-10-14 17:44:03 +0700

    error postitem.js

--- src/component/post/post-item/PostItem.jsx
+++ src/component/post/post-item/PostItem.jsx
@@ -1,5 +1,30 @@
+import { Avatar, Flex, Group, Loader, Text } from '@mantine/core';
+import { useAuthStore } from '../../../store/authStore';
+import { timeAgo } from '../../../util/convertTime';
+
 function PostItem() {
-    return <div>PostItem</div>;
+    const { user } = useAuthStore();
+
+    // const { data, loading } = useFetchData('/posts', []);
+    console.log(data);
+
+    return (
+        <Group>
+            <Flex>
+                <Avatar src={user.avatar_url} size={'md'} name={user.username} color='initials' />
+                {loading ? (
+                    <Group w={'100%'} justify='center' align='center'>
+                        <Loader type='bars' />
+                    </Group>
+                ) : (
+                    <Group>
+                        <Text>{user.username}</Text>
+                        <Text>{timeAgo()}</Text>
+                    </Group>
+                )}
+            </Flex>
+        </Group>
+    );
 }
 
 export default PostItem;
